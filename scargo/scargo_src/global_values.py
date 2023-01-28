9574f75f73ccb088a4a844a69ca76ff1441cea92
Andrzej Aksenczuuk <96312831+aaksenczuk@users.noreply.github.com>
2023-01-28 12:58:05 +0100

    Env/doc autodeploy (#6)
    * release on PR close
    * update ci

--- scargo/scargo_src/global_values.py
+++ scargo/scargo_src/global_values.py
@@ -6,7 +6,7 @@
 import pkgutil
 from pathlib import Path
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 DESCRIPTION = "C/C++ package and software development life cycle manager based on RUST cargo idea."
 
