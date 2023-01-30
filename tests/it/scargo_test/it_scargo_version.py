b928ae668e96b7b8fa7045ac7305f5cfe90a7135
Jacek Wroblewski <job@spyro-soft.com>
2023-01-30 13:13:22 +0100

    new test for scargo version command

---
+++ tests/it/scargo_test/it_scargo_version.py
@@ -0,0 +1,23 @@
+import pytest
+from typer.testing import CliRunner
+
+from scargo import cli
+
+
+PRECONDITIONS = [
+    "precondition_regression_tests",
+    "precondition_regular_tests",
+    "precondition_regression_tests_esp32",
+    "precondition_regression_tests_stm32",
+]
+
+
+@pytest.mark.parametrize("precondition", PRECONDITIONS)
+def test_version(precondition, request):
+    request.getfixturevalue(precondition)
+    runner = CliRunner()
+
+    result = runner.invoke(cli, ["version"])
+
+    assert result.exit_code == 0
+    assert "scargo version: " in result.output
