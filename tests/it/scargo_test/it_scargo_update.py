4a483baa242da2ff878ca2af73a9674848dcd2be
Jacek Wroblewski <job@spyro-soft.com>
2023-01-31 14:40:15 +0100

    new tests to scargo update command test_update_profile_additional_variables test_update_add_profile

--- tests/it/scargo_test/it_scargo_update.py
+++ tests/it/scargo_test/it_scargo_update.py
@@ -2,6 +2,7 @@ import pytest
 from typer.testing import CliRunner
 
 from scargo import cli
+from utils import add_cmake_variables_to_toml, add_profile_to_toml
 
 PRECONDITIONS = [
     "precondition_regression_tests",
@@ -43,3 +44,30 @@ def test_update_with_config_arg_fail(precondition, request):
     runner = CliRunner()
     result = runner.invoke(cli, ["update", "--config-file=non/existing/path"])
     assert result.exit_code == 2
+
+
+@pytest.mark.parametrize("precondition", PRECONDITIONS)
+def test_update_profile_additional_variables(precondition, request):
+    request.getfixturevalue(precondition)
+    runner = CliRunner()
+
+    add_cmake_variables_to_toml("extra_var", "debug_extra")
+    result = runner.invoke(cli, ["update"])
+    assert result.exit_code == 0
+
+    cmakelists_lines = [line.strip() for line in open("CMakeLists.txt").readlines()]
+    assert "SET(extra_var debug_extra)" in cmakelists_lines
+
+
+@pytest.mark.parametrize("precondition", PRECONDITIONS)
+def test_update_add_profile(precondition, request):
+    request.getfixturevalue(precondition)
+    runner = CliRunner()
+
+    add_profile_to_toml("new", "cflags", 'cxxflags',"cflags for new profile", 'cxxflags for new profile')
+    result = runner.invoke(cli, ["update"])
+    assert result.exit_code == 0
+    cmakelists_lines = [line.strip() for line in open("CMakeLists.txt").readlines()]
+    print(cmakelists_lines)
+    assert 'set(CMAKE_C_FLAGS_NEW   "cflags for new profile")' in cmakelists_lines
+    assert 'set(CMAKE_CXX_FLAGS_NEW "cxxflags for new profile")' in cmakelists_lines
--- tests/it/scargo_test/utils.py
+++ tests/it/scargo_test/utils.py
@@ -31,3 +31,30 @@ def remove_dockerfile_path_from_toml_file(toml_path: Path = Path("scargo.toml"))
 
     with toml_path.open("w") as f:
         toml.dump(data, f)
+
+
+def add_cmake_variables_to_toml(
+    var: str, value: str, toml_path: Path = Path("scargo.toml")
+):
+    data = toml.load(toml_path)
+
+    data["profile"]["Debug"][var] = value
+
+    with toml_path.open("w") as f:
+        toml.dump(data, f)
+
+
+def add_profile_to_toml(
+    profile: str,
+    var: str,
+    var2: str,
+    value: str,
+    value2: str,
+    toml_path: Path = Path("scargo.toml"),
+):
+    data = toml.load(toml_path)
+    temp_dict = dict()
+    temp_dict[profile] = {var: value, var2: value2}
+    data["profile"].update(temp_dict)
+    with toml_path.open("w") as f:
+        toml.dump(data, f)
