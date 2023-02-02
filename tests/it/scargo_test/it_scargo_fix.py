d209427d3be5dc056323ab1ee267bcbc4badc190
Patryk Parylo <ppa@spyro-soft.com>
2023-02-02 10:50:25 +0100

    Add AAA template to tests

--- tests/it/scargo_test/it_scargo_fix.py
+++ tests/it/scargo_test/it_scargo_fix.py
@@ -14,16 +14,25 @@ FIX_OPTIONS = ["--pragma", "--copyright", "--clang-format", "-h", "--help"]
 
 @pytest.mark.parametrize("precondition", PRECONDITIONS)
 def test_fix(precondition, request):
+    # ARRANGE
     request.getfixturevalue(precondition)
-
     runner = CliRunner()
+
+    # ACT
     result = runner.invoke(cli, ["fix"])
+
+    # ASSERT
     assert result.exit_code == 0
 
 
 def test_fix_caps_fail():
+    # ARRANGE
     runner = CliRunner()
+
+    # ACT
     result = runner.invoke(cli, ["FIX"])
+
+    # ASSERT
     assert result.exit_code == 2
     assert "Error: No such command" in result.output
 
@@ -31,8 +40,13 @@ def test_fix_caps_fail():
 @pytest.mark.parametrize("precondition", PRECONDITIONS)
 @pytest.mark.parametrize("fix_option", FIX_OPTIONS)
 def test_fix_option(precondition, request, fix_option):
+    # ARRANGE
     request.getfixturevalue(precondition)
-
     runner = CliRunner()
+
+    # ACT
     result = runner.invoke(cli, ["fix", fix_option])
+
+    # ASSERT
     assert result.exit_code == 0
+    
