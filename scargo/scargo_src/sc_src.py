7573e39f9790b6e3f97a4d16f2a41e4ae5d00e25
Jan Szejko <jze@spyro-soft.com>
2023-02-23 16:52:31 +0100

    refactor check functions in sc_src

--- scargo/scargo_src/sc_src.py
+++ scargo/scargo_src/sc_src.py
@@ -6,8 +6,9 @@ import glob
 import os
 import subprocess
 import sys
+from itertools import chain
 from pathlib import Path
-from typing import List, Optional, Tuple
+from typing import Iterable, List, Optional, Sequence, Tuple
 
 import docker as dock
 import tomlkit
@@ -23,8 +24,6 @@ from scargo.scargo_src.sc_config import (
 )
 from scargo.scargo_src.sc_logger import get_logger
 from scargo.scargo_src.utils import get_config_file_path, get_project_root
-
-###############################################################################
 
 
 def run_scargo_again_in_docker(project_config: ProjectConfig) -> None:
@@ -68,9 +67,6 @@ def run_scargo_again_in_docker(project_config: ProjectConfig) -> None:
         sys.exit(result["StatusCode"])
 
 
-###############################################################################
-
-
 def check_scargo_version(config: Config) -> None:
     """
     Check scargo version
@@ -85,9 +81,6 @@ def check_scargo_version(config: Config) -> None:
         logger = get_logger()
         logger.warning("Warning: scargo package is different then in lock file")
         logger.info("Run scargo update")
-
-
-###############################################################################
 
 
 def get_scargo_config_or_exit(
@@ -112,9 +105,6 @@ def get_scargo_config_or_exit(
         sys.exit(1)
 
 
-###############################################################################
-
-
 def add_version_to_scargo_lock() -> None:
     """
     :return: project configuration as dict
@@ -132,9 +122,6 @@ def add_version_to_scargo_lock() -> None:
     config.setdefault("scargo", tomlkit.table())["version"] = ver
     scargo_lock_file = open(scargo_lock, "w")
     tomlkit.dump(config, scargo_lock_file)
-
-
-###############################################################################
 
 
 def get_docker_files_from_scargo_pkg(directory: Path, target: Target) -> List[str]:
@@ -155,9 +142,6 @@ def get_docker_files_from_scargo_pkg(directory: Path, target: Target) -> List[st
     return result_list_of_files
 
 
-###############################################################################
-
-
 def check_pragma(config: Config, fix_errors: bool) -> None:
     """
     Private function used in commands `scargo check` and `scargo fix`.
@@ -170,57 +154,32 @@ def check_pragma(config: Config, fix_errors: bool) -> None:
     logger = get_logger()
     logger.info("Starting pragma check...")
 
-    file_extensions = (".h", ".hpp")
     error_counter = 0
-    source_dir = config.project.target.source_dir
 
-    exclude_list = []
+    for file_path in find_files(config.check.pragma.exclude, config, headers_only=True):
+        found_pragma_once = False
 
-    # Collect global excludes.
-    for pattern in config.check.exclude:
-        exclude_list.extend(glob.glob(pattern))
-
-    # Collect local excludes.
-    for pattern in config.check.pragma.exclude:
-        exclude_list.extend(glob.glob(pattern))
-
-    for root, _, filenames in os.walk(source_dir):
-        for fname in filenames:
-            fname = os.path.join(root, fname)
-
-            exist_flag = False
-            for exclude in exclude_list:
-                if exclude in fname:
-                    logger.info("Skipping %s", fname)
-                    exist_flag = True
+        with open(file_path, encoding="utf-8") as file:
+            print(file_path)
+            for line in file.readlines():
+                if "#pragma once" in line:
+                    found_pragma_once = True
                     break
-            if exist_flag:
-                continue
 
-            if fname.lower().endswith(file_extensions):
-                found_pragma_once = False
+        if not found_pragma_once:
+            error_counter += 1
+            logger.warning("Missing pragma in %s", file_path)
 
-                with open(fname, encoding="utf-8") as file:
-                    print(fname)
-                    for line in file.readlines():
-                        if "#pragma once" in line:
-                            found_pragma_once = True
-                            break
+            if fix_errors:
+                logger.info("Fixing...")
 
-                if not found_pragma_once:
-                    error_counter += 1
-                    logger.warning("Missing pragma in %s", fname)
+                with open(file_path, encoding="utf-8") as file:
+                    old = file.read()
 
-                    if fix_errors:
-                        logger.info("Fixing...")
-
-                        with open(fname, encoding="utf-8") as file:
-                            old = file.read()
-
-                        with open(fname, "w", encoding="utf-8") as file:
-                            file.write("#pragma once\n")
-                            file.write("\n")
-                            file.write(old)
+                with open(file_path, "w", encoding="utf-8") as file:
+                    file.write("#pragma once\n")
+                    file.write("\n")
+                    file.write(old)
 
     if fix_errors:
         logger.info("Finished pragma check. Fixed problems in %s files.", error_counter)
@@ -229,9 +188,6 @@ def check_pragma(config: Config, fix_errors: bool) -> None:
         if error_counter > 0:
             logger.error("pragma check fail!")
             sys.exit(1)
-
-
-###############################################################################
 
 
 def check_copyright(config: Config, fix_errors: bool) -> None:
@@ -245,85 +201,52 @@ def check_copyright(config: Config, fix_errors: bool) -> None:
     logger = get_logger()
     logger.info("Starting copyright check...")
 
-    file_extensions = (".h", ".hpp", ".c", ".cpp")
     error_counter = 0
-    source_dir = config.project.target.source_dir
 
-    exclude_list = []
     copyright_desc = config.check.copyright.description
     if not copyright_desc:
         logger.warning("No copyrights in defined in toml")
         return
 
-    # Collect global excludes.
-    for pattern in config.check.exclude:
-        exclude_list.extend(glob.glob(pattern))
+    for file_path in find_files(config.check.copyright.exclude, config):
+        found_copyright = False
+        any_copyright = False
 
-    # Collect local excludes.
-    for pattern in config.check.copyright.exclude:
-        exclude_list.extend(glob.glob(pattern))
-
-    for root, _, filenames in os.walk(source_dir):
-        for fname in filenames:
-            fname = os.path.join(root, fname)
-
-            exist_flag = False
-            for exclude in exclude_list:
-                if exclude in fname:
-                    logger.info("Skipping %s", fname)
-                    exist_flag = True
+        with open(file_path, encoding="utf-8") as file:
+            for line in file.readlines():
+                if copyright_desc in line:
+                    found_copyright = True
                     break
-            if exist_flag:
-                continue
+                if "copyright" in line.lower():
+                    any_copyright = True
+                    break
 
-            if fname.lower().endswith(file_extensions):
-                found_copyright = False
-                any_copyright = False
+        if not found_copyright:
+            error_counter += 1
+            if any_copyright:
+                logger.warning("Incorrect and not excluded copyright in %s", file_path)
+            else:
+                logger.info("Missing copyright in %s.", file_path)
 
-                with open(fname, encoding="utf-8") as file:
-                    for line in file.readlines():
-                        if copyright_desc in line:
-                            found_copyright = True
-                            break
-                        if "copyright" in line.lower():
-                            any_copyright = True
-                            break
+            if fix_errors and not any_copyright:
+                logger.info("Fixing...")
+                with open(file_path, encoding="utf-8") as file:
+                    old = file.read()
 
-                if not found_copyright:
-                    error_counter += 1
-                    if any_copyright:
-                        logger.warning(
-                            "Incorrect and not excluded copyright in %s", fname
-                        )
-                    else:
-                        logger.info("Missing copyright in %s.", fname)
-
-                    if fix_errors and not any_copyright:
-                        logger.info("Fixing...")
-                        with open(fname, encoding="utf-8") as file:
-                            old = file.read()
-
-                        with open(fname, "w", encoding="utf-8") as file:
-                            file.write("//\n")
-                            file.write(f"// {copyright_desc}\n")
-                            file.write("//\n")
-                            file.write("\n")
-                            file.write(old)
+                with open(file_path, "w", encoding="utf-8") as file:
+                    file.write("//\n")
+                    file.write(f"// {copyright_desc}\n")
+                    file.write("//\n")
+                    file.write("\n")
+                    file.write(old)
 
     if fix_errors:
-        logger.info(
-            "Finished copyright check. Fixed problems in %s files.", error_counter
-        )
+        logger.info("Finished pragma check. Fixed problems in %s files.", error_counter)
     else:
-        logger.info(
-            "Finished copyright check. Found problems in %s files.", error_counter
-        )
+        logger.info("Finished pragma check. Found problems in %s files.", error_counter)
         if error_counter > 0:
-            logger.error("copyright check fail!")
+            logger.error("pragma check fail!")
             sys.exit(1)
-
-
-###############################################################################
 
 
 def check_todo(config: Config) -> None:
@@ -338,52 +261,22 @@ def check_todo(config: Config) -> None:
     logger.info("Starting todo check...")
 
     keywords = ("tbd", "todo", "TODO", "fixme")
-    file_extensions = (".h", ".hpp", ".c", ".cpp")
     error_counter = 0
-    source_dir = config.project.target.source_dir
 
-    exclude_list = []
-
-    # Collect global excludes.
-    for pattern in config.check.exclude:
-        exclude_list.extend(glob.glob(pattern))
-
-    # Collect local excludes.
-    for pattern in config.check.todo.exclude:
-        exclude_list.extend(glob.glob(pattern))
-
-    for root, _, filenames in os.walk(source_dir):
-        for fname in filenames:
-            fname = os.path.join(root, fname)
-
-            exist_flag = False
-            for exclude in exclude_list:
-                if exclude in fname:
-                    logger.info("Skipping %s", fname)
-                    exist_flag = True
-                    break
-            if exist_flag:
-                continue
-
-            if fname.lower().endswith(file_extensions):
-                with open(fname, encoding="utf-8") as file:
-                    line_number = 0
-                    for line in file.readlines():
-                        line_number += 1
-                        for keyword in keywords:
-                            if keyword in line:
-                                error_counter += 1
-                                logger.warning(
-                                    f"Found {keyword} in {fname} at line {line_number}"
-                                )
+    for file_path in find_files(config.check.todo.exclude, config):
+        with open(file_path, encoding="utf-8") as file:
+            for line_number, line in enumerate(file.readlines(), start=1):
+                for keyword in keywords:
+                    if keyword in line:
+                        error_counter += 1
+                        logger.warning(
+                            f"Found {keyword} in {file_path} at line {line_number}"
+                        )
 
     logger.info("Finished todo check. Found %s problems.", error_counter)
     if error_counter > 0:
         logger.error("todo check fail!")
         sys.exit(1)
-
-
-###############################################################################
 
 
 def check_clang_format(config: Config, fix_errors: bool, verbose: bool) -> None:
@@ -398,50 +291,25 @@ def check_clang_format(config: Config, fix_errors: bool, verbose: bool) -> None:
     logger = get_logger()
     logger.info("Starting clang-format check...")
 
-    file_extensions = (".h", ".hpp", ".c", ".cpp")
     error_counter = 0
-    source_dir = config.project.target.source_dir
 
-    exclude_list = []
+    for file_path in find_files(config.check.clang_format.exclude, config):
+        cmd = "clang-format -style=file --dry-run " + str(file_path)
+        out = subprocess.getoutput(cmd)
 
-    # Collect global excludes.
-    for pattern in config.check.exclude:
-        exclude_list.extend(glob.glob(pattern))
+        if out != "":
+            error_counter += 1
 
-    # Collect local excludes.
-    for pattern in config.check.clang_format.exclude:
-        exclude_list.extend(glob.glob(pattern))
+            if verbose:
+                logger.info(out)
+            else:
+                logger.warning("clang-format found error in file %s", file_path)
 
-    for root, _, filenames in os.walk(source_dir):
-        for fname in filenames:
-            fname = os.path.join(root, fname)
+            if fix_errors:
+                logger.info("Fixing...")
 
-            exist_flag = False
-            for exclude in exclude_list:
-                if exclude in fname:
-                    logger.info("Skipping %s", fname)
-                    exist_flag = True
-                    break
-            if exist_flag:
-                continue
-
-            if fname.lower().endswith(file_extensions):
-                cmd = "clang-format -style=file --dry-run " + fname
-                out = subprocess.getoutput(cmd)
-
-                if out != "":
-                    error_counter += 1
-
-                    if verbose:
-                        logger.info(out)
-                    else:
-                        logger.warning("clang-format found error in file %s", fname)
-
-                    if fix_errors:
-                        logger.info("Fixing...")
-
-                        cmd = "clang-format -style=file -i " + fname
-                        subprocess.check_call(cmd, shell=True)
+                cmd = "clang-format -style=file -i " + str(file_path)
+                subprocess.check_call(cmd, shell=True)
 
     if fix_errors:
         logger.info(
@@ -456,9 +324,6 @@ def check_clang_format(config: Config, fix_errors: bool, verbose: bool) -> None:
             sys.exit(1)
 
 
-###############################################################################
-
-
 def check_clang_tidy(config: Config, verbose: bool) -> None:
     """
     Private function used in command `scargo check`.
@@ -470,9 +335,31 @@ def check_clang_tidy(config: Config, verbose: bool) -> None:
     logger = get_logger()
     logger.info("Starting clang-tidy check...")
 
-    file_extensions = (".h", ".hpp", ".c", ".cpp")
     error_counter = 0
-    source_dir = config.project.target.source_dir
+
+    for file_path in find_files(config.check.clang_tidy.exclude, config):
+        cmd = "clang-tidy " + str(file_path) + " --assume-filename=.hxx --"
+        out = subprocess.getoutput(cmd)
+
+        if "error:" in out:
+            error_counter += 1
+
+            if verbose:
+                logger.info(out)
+            else:
+                logger.warning("clang-tidy found error in file %s", file_path)
+
+    logger.info("Finished clang-tidy check. Found problems in %s files.", error_counter)
+    if error_counter > 0:
+        logger.error("clang-tidy check fail!")
+        sys.exit(1)
+
+
+def find_files(
+    exclude_patterns: Sequence[str], config: Config, headers_only: bool = False
+) -> Iterable[Path]:
+    logger = get_logger()
+    source_dir = Path(config.project.target.source_dir)
 
     exclude_list = []
 
@@ -481,41 +368,21 @@ def check_clang_tidy(config: Config, verbose: bool) -> None:
         exclude_list.extend(glob.glob(pattern))
 
     # Collect local excludes.
-    for pattern in config.check.clang_tidy.exclude:
+    for pattern in exclude_patterns:
         exclude_list.extend(glob.glob(pattern))
 
-    for root, _, filenames in os.walk(source_dir):
-        for fname in filenames:
-            fname = os.path.join(root, fname)
-
-            exist_flag = False
-            for exclude in exclude_list:
-                if exclude in fname:
-                    logger.info("Skipping %s", fname)
-                    exist_flag = True
-                    break
-            if exist_flag:
+    glob_patterns = (
+        ("*.h", "*.hpp") if headers_only else ("*.h", "*.hpp", "*.c", "*.cpp")
+    )
+    for file_path in chain.from_iterable(
+        source_dir.rglob(pattern) for pattern in glob_patterns
+    ):
+        if file_path.is_file():
+            if any(exclude in str(file_path) for exclude in exclude_list):
+                logger.info("Skipping %s", file_path)
                 continue
 
-            if fname.lower().endswith(file_extensions):
-                cmd = "clang-tidy " + fname + " --assume-filename=.hxx --"
-                out = subprocess.getoutput(cmd)
-
-                if "error:" in out:
-                    error_counter += 1
-
-                    if verbose:
-                        logger.info(out)
-                    else:
-                        logger.warning("clang-tidy found error in file %s", fname)
-
-    logger.info("Finished clang-tidy check. Found problems in %s files.", error_counter)
-    if error_counter > 0:
-        logger.error("clang-tidy check fail!")
-        sys.exit(1)
-
-
-###############################################################################
+            yield file_path
 
 
 def check_cyclomatic(config: Config) -> None:
@@ -549,9 +416,6 @@ def check_cyclomatic(config: Config) -> None:
     logger.info("Finished cyclomatic check.")
 
 
-###############################################################################
-
-
 def check_cppcheck() -> None:
     """
     Private function used in command `scargo check`.
@@ -567,9 +431,6 @@ def check_cppcheck() -> None:
     except subprocess.CalledProcessError:
         logger.error("cppcheck fail")
     logger.info("Finished cppcheck check.")
-
-
-###############################################################################
 
 
 def prepare_config() -> Config:
