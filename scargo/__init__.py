0fd227c26efa3488795620aa0bde49b24fd7005d
Jan Szejko <jze@spyro-soft.com>
2023-02-27 15:28:34 +0100

    remove leftover imports

--- scargo/__init__.py
+++ scargo/__init__.py
@@ -1,10 +1,4 @@
 # #
 # @copyright Copyright (C) 2023 SpyroSoft Solutions S.A. All rights reserved.
 # #
-import sys
-from pathlib import Path
-from typing import List, Optional
-
-from typer import Argument, Context, Option, Typer
-
 __version__ = "1.0.6"
