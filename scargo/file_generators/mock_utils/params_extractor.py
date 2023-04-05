bc7b2490c8f2c05a11a1d3bd0bb092d768569720
Jan Szejko <jze@spyro-soft.com>
2023-04-05 15:57:51 +0200

    Refactor params_extractor

--- scargo/file_generators/mock_utils/params_extractor.py
+++ scargo/file_generators/mock_utils/params_extractor.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List
 
 from clang.cindex import Cursor, CursorKind
 
@@ -12,7 +12,7 @@ from scargo.file_generators.mock_utils.data_classes import (
 
 def extract_namespaces(cursor: Cursor, filename: str) -> List[MockNamespaceDescriptor]:
     return [
-        _extract_namespace_params(descendant)
+        MockNamespaceDescriptor(descendant.spelling)
         for descendant in cursor.walk_preorder()
         if descendant.kind == CursorKind.NAMESPACE
         and descendant.location.file == filename
@@ -21,47 +21,41 @@ def extract_namespaces(cursor: Cursor, filename: str) -> List[MockNamespaceDescr
 
 def extract_classes(cursor: Cursor, filename: str) -> List[MockClassDescriptor]:
     return [
-        _extract_class_params(descendant)
+        MockClassDescriptor(
+            cursor.spelling, f"Mock{cursor.spelling}", _extract_cxx_methods(cursor)
+        )
         for descendant in cursor.walk_preorder()
         if descendant.kind == CursorKind.CLASS_DECL
         and descendant.location.file == filename
     ]
 
 
-def _extract_namespace_params(cursor: Cursor) -> MockNamespaceDescriptor:
-    return MockNamespaceDescriptor(cursor.spelling)
-
-
-def _extract_class_params(cursor: Cursor) -> MockClassDescriptor:
-    methods = _extract_cxx_methods(cursor)
-    return MockClassDescriptor(cursor.spelling, f"Mock{cursor.spelling}", methods)
-
-
 def _extract_cxx_methods(cursor: Cursor) -> List[MockFunctionDescriptor]:
-    descriptors = (
+    return [
         _extract_method_params(descendant)
         for descendant in cursor.walk_preorder()
         if descendant.kind == CursorKind.CXX_METHOD
-    )
-
-    return [x for x in descriptors if x is not None]
+        and cursor.access_specifier.name == "PUBLIC"
+    ]
 
 
 def _extract_method_params(
     cursor: Cursor,
-) -> Optional[MockFunctionDescriptor]:
-    fun_ret_val = cursor.result_type.spelling
-    fun_name = cursor.spelling
-    fun_args = []
+) -> MockFunctionDescriptor:
     specifiers = []
-
-    if cursor.access_specifier.name != "PUBLIC":
-        return None
     if cursor.is_virtual_method():
         specifiers.append("override")
     if cursor.is_const_method():
         specifiers.append("const")
-    for i in cursor.walk_preorder():
-        if i.kind == CursorKind.PARM_DECL:
-            fun_args.append(ArgumentDescriptor(i.spelling, i.type.spelling))
-    return MockFunctionDescriptor(fun_name, fun_ret_val, specifiers, arguments=fun_args)
+
+    fun_args = [
+        ArgumentDescriptor(descendant.spelling, descendant.type.spelling)
+        for descendant in cursor.walk_preorder()
+        if descendant.kind == CursorKind.PARM_DECL
+    ]
+    return MockFunctionDescriptor(
+        name=cursor.spelling,
+        return_type=cursor.result_type.spelling,
+        specifiers=specifiers,
+        arguments=fun_args,
+    )
