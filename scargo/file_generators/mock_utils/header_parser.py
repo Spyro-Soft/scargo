a978000bcdd506e66ecc20f2b28d510874754815
Jan Szejko <jze@spyro-soft.com>
2023-04-05 15:24:53 +0200

    Eliminate unnecessary class

--- scargo/file_generators/mock_utils/header_parser.py
+++ scargo/file_generators/mock_utils/header_parser.py
@@ -3,7 +3,10 @@ from pathlib import Path
 from clang.cindex import Index
 
 from scargo.file_generators.mock_utils.data_classes import HeaderDescriptor
-from scargo.file_generators.mock_utils.params_extractor import ParamsExtractor
+from scargo.file_generators.mock_utils.params_extractor import (
+    extract_classes,
+    extract_namespaces,
+)
 
 
 def parse_file(file_path: Path) -> HeaderDescriptor:
@@ -15,17 +18,9 @@ def parse_file(file_path: Path) -> HeaderDescriptor:
     dict of classes where keys are "public", "private" and "mock_class_name"
     and list of namespaces, i.e. strings that start with "namespace".
     """
-    file_name = file_path.name
-
-    hdr = HeaderDescriptor(file_name)
-
     idx = Index.create()
     translation_unit = idx.parse(str(file_path), ["-x", "c++"])
 
-    hdr.namespaces = ParamsExtractor.extract_namespaces(
-        translation_unit.cursor, str(file_path)
-    )
-    hdr.classes = ParamsExtractor.extract_classes(
-        translation_unit.cursor, str(file_path)
-    )
-    return hdr
+    namespaces = extract_namespaces(translation_unit.cursor, str(file_path))
+    classes = extract_classes(translation_unit.cursor, str(file_path))
+    return HeaderDescriptor(name=file_path.name, namespaces=namespaces, classes=classes)
--- scargo/file_generators/mock_utils/params_extractor.py
+++ scargo/file_generators/mock_utils/params_extractor.py
@@ -10,65 +10,59 @@ from scargo.file_generators.mock_utils.data_classes import (
 )
 
 
-class ParamsExtractor:
-    @staticmethod
-    def extract_cxx_methods(cursor: Cursor) -> List[MockFunctionDescriptor]:
-        descriptors = (
-            ParamsExtractor._extract_method_params(descendant)
-            for descendant in cursor.walk_preorder()
-            if descendant.kind == CursorKind.CXX_METHOD
-        )
+def _extract_cxx_methods(cursor: Cursor) -> List[MockFunctionDescriptor]:
+    descriptors = (
+        _extract_method_params(descendant)
+        for descendant in cursor.walk_preorder()
+        if descendant.kind == CursorKind.CXX_METHOD
+    )
 
-        return [x for x in descriptors if x is not None]
+    return [x for x in descriptors if x is not None]
 
-    @staticmethod
-    def extract_namespaces(
-        cursor: Cursor, filename: str
-    ) -> List[MockNamespaceDescriptor]:
-        return [
-            ParamsExtractor._extract_namespace_params(descendant)
-            for descendant in cursor.walk_preorder()
-            if descendant.kind == CursorKind.NAMESPACE
-            and descendant.location.file == filename
-        ]
 
-    @staticmethod
-    def extract_classes(cursor: Cursor, filename: str) -> List[MockClassDescriptor]:
-        return [
-            ParamsExtractor._extract_class_params(descendant)
-            for descendant in cursor.walk_preorder()
-            if descendant.kind == CursorKind.CLASS_DECL
-            and descendant.location.file == filename
-        ]
+def extract_namespaces(cursor: Cursor, filename: str) -> List[MockNamespaceDescriptor]:
+    return [
+        _extract_namespace_params(descendant)
+        for descendant in cursor.walk_preorder()
+        if descendant.kind == CursorKind.NAMESPACE
+        and descendant.location.file == filename
+    ]
 
-    @staticmethod
-    def _extract_method_params(
-        cursor: Cursor,
-    ) -> Optional[MockFunctionDescriptor]:
-        fun_ret_val = cursor.result_type.spelling
-        fun_name = cursor.spelling
-        fun_args = []
-        specifiers = []
 
-        if cursor.access_specifier.name != "PUBLIC":
-            return None
-        if cursor.is_virtual_method():
-            specifiers.append("override")
-        if cursor.is_const_method():
-            specifiers.append("const")
-        for i in cursor.walk_preorder():
-            if i.kind == CursorKind.PARM_DECL:
-                fun_args.append(ArgumentDescriptor(i.spelling, i.type.spelling))
-        return MockFunctionDescriptor(
-            fun_name, fun_ret_val, specifiers, arguments=fun_args
-        )
+def extract_classes(cursor: Cursor, filename: str) -> List[MockClassDescriptor]:
+    return [
+        _extract_class_params(descendant)
+        for descendant in cursor.walk_preorder()
+        if descendant.kind == CursorKind.CLASS_DECL
+        and descendant.location.file == filename
+    ]
 
-    @staticmethod
-    def _extract_namespace_params(cursor: Cursor) -> MockNamespaceDescriptor:
-        return MockNamespaceDescriptor(cursor.spelling)
 
-    @staticmethod
-    def _extract_class_params(cursor: Cursor) -> MockClassDescriptor:
-        cls = MockClassDescriptor(cursor.spelling, f"Mock{cursor.spelling}")
-        cls.methods = ParamsExtractor.extract_cxx_methods(cursor)
-        return cls
+def _extract_method_params(
+    cursor: Cursor,
+) -> Optional[MockFunctionDescriptor]:
+    fun_ret_val = cursor.result_type.spelling
+    fun_name = cursor.spelling
+    fun_args = []
+    specifiers = []
+
+    if cursor.access_specifier.name != "PUBLIC":
+        return None
+    if cursor.is_virtual_method():
+        specifiers.append("override")
+    if cursor.is_const_method():
+        specifiers.append("const")
+    for i in cursor.walk_preorder():
+        if i.kind == CursorKind.PARM_DECL:
+            fun_args.append(ArgumentDescriptor(i.spelling, i.type.spelling))
+    return MockFunctionDescriptor(fun_name, fun_ret_val, specifiers, arguments=fun_args)
+
+
+def _extract_namespace_params(cursor: Cursor) -> MockNamespaceDescriptor:
+    return MockNamespaceDescriptor(cursor.spelling)
+
+
+def _extract_class_params(cursor: Cursor) -> MockClassDescriptor:
+    cls = MockClassDescriptor(cursor.spelling, f"Mock{cursor.spelling}")
+    cls.methods = _extract_cxx_methods(cursor)
+    return cls
