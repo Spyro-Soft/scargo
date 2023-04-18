e1e8c18d7caab54c92637569a71a27c6d3e1adfd
Jan Szejko <jze@spyro-soft.com>
2023-04-18 13:43:32 +0200

    Update types-clang

--- scargo/clang_utils.py
+++ scargo/clang_utils.py
@@ -14,8 +14,6 @@ def get_comment_lines(file_path: Path) -> Iterable[Tuple[int, str]]:
     for token in translation_unit.cursor.get_tokens():
         if token.kind == TokenKind.COMMENT:  # pylint: disable=no-member
             yield from enumerate(
-                # there's a bug in types-clang: https://github.com/tgockel/types-clang/issues/8
-                # when it's fixed in a new release, we can remove the type ignore comment
                 token.spelling.splitlines(keepends=False),
-                start=token.extent.start.line,  # type: ignore[attr-defined]
+                start=token.extent.start.line,
             )
