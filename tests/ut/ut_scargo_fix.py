968b15ef5e9fedc03f8eda884e0b1057b84ad8a2
Jacek Wroblewski <job@spyro-soft.com>
2023-04-25 11:05:56 +0200

    UT tests for scargo_fix

---
+++ tests/ut/ut_scargo_fix.py
@@ -0,0 +1,44 @@
+from unittest.mock import MagicMock
+
+import pytest
+from pytest_mock import MockerFixture
+
+from scargo.commands.fix import scargo_fix
+from scargo.config import Config
+
+
+def test_scargo_fix_pragma(
+    mock_prepare_config: Config,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    scargo_fix(True, True, True)
+    assert "Fixed" in caplog.text
+
+
+def test_scargo_fix_copyright(
+    mock_prepare_config: Config,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    scargo_fix(True, True, True)
+    assert "Fixed" in caplog.text
+
+
+def test_scargo_fix_clang_format(
+    mock_prepare_config: Config,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    scargo_fix(True, True, True)
+    assert "Fixed" in caplog.text
+
+
+def test_fix_none_option(
+    mock_prepare_config: Config,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    scargo_fix(False, False, False)
+    assert "Fixed" in caplog.text
+
+
+@pytest.fixture
+def mock_prepare_config(mocker: MockerFixture, config: Config) -> MagicMock:
+    return mocker.patch(f"{scargo_fix.__module__}.prepare_config", return_value=config)
