3cfd71710763a6733dfb113410d7db47b83f404e
kta <kta@spyro-soft.com>
2023-09-07 15:34:09 +0200

    Fix clang tidy check
    Fix unit tests
    
    Review comment fix
    
    Fix integration tests
    
    Fix IT tests

--- tests/ut/scargo_check/ut_clang_tidy_checker.py
+++ tests/ut/scargo_check/ut_clang_tidy_checker.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Tuple
 from unittest.mock import MagicMock
 
@@ -8,19 +9,31 @@ from scargo.commands.check import ClangTidyChecker
 from scargo.config import Config
 from tests.ut.utils import get_log_data
 
-CLANG_TIDY_COMMAND = ["clang-tidy", "foo/bar.hpp", "--"]
+CLANG_TIDY_COMMAND = ["clang-tidy", "foo/bar.hpp"]
 
 CLANG_TIDY_NORMAL_OUTPUT = "everything is tidy!"
 CLANG_TIDY_ERROR_OUTPUT = "error: something is not tidy!"
 
 
+@pytest.mark.parametrize(
+    "build_path_str",
+    ["build/Debug", "build/Release", "build/RelWithDebInfo", "build/MinSizeRel"],
+)
 def test_check_clang_tidy_pass(
+    build_path_str: str,
     caplog: pytest.LogCaptureFixture,
     config: Config,
     mock_find_files: MagicMock,
     fake_process: FakeProcess,
 ) -> None:
-    fake_process.register(CLANG_TIDY_COMMAND, stdout=CLANG_TIDY_NORMAL_OUTPUT)
+    build_path = Path(build_path_str)
+    build_path.mkdir(parents=True)
+    compilation_db_path = Path(build_path, "compile_commands.json")
+    compilation_db_path.touch(exist_ok=True)
+
+    fake_process.register(
+        CLANG_TIDY_COMMAND + ["-p", build_path_str], stdout=CLANG_TIDY_NORMAL_OUTPUT
+    )
     result = ClangTidyChecker(config).check()
     assert result == 0
     assert all(
@@ -43,9 +56,38 @@ def test_check_clang_tidy_fail(
     mock_find_files: MagicMock,
     fake_process: FakeProcess,
 ) -> None:
+    build_path = Path("build/Debug")
+    build_path.mkdir(parents=True)
+    compilation_db_path = Path(build_path, "compile_commands.json")
+    compilation_db_path.touch(exist_ok=True)
     fake_process.register(
-        CLANG_TIDY_COMMAND, stdout=CLANG_TIDY_ERROR_OUTPUT, returncode=1
+        CLANG_TIDY_COMMAND + ["-p", build_path],
+        stdout=CLANG_TIDY_ERROR_OUTPUT,
+        returncode=1,
     )
     result = ClangTidyChecker(config, verbose=verbose).check()
     assert result == 1
     assert expected_message in get_log_data(caplog.records)
+
+
+@pytest.mark.parametrize(
+    "build_path_str",
+    ["build1", "build/InvalidBuildType", "build/Debug"],
+)
+def test_check_clang_tidy_fail_wrong_dirs(
+    build_path_str: str,
+    caplog: pytest.LogCaptureFixture,
+    config: Config,
+    mock_find_files: MagicMock,
+    fake_process: FakeProcess,
+) -> None:
+    build_path = Path(build_path_str)
+    build_path.mkdir(parents=True)
+    # Expect clang-tidy command not to be called if path to compilation db is wrong:
+    fake_process.register(
+        CLANG_TIDY_COMMAND + ["-p", build_path],
+        stdout=CLANG_TIDY_ERROR_OUTPUT,
+        occurrences=0,
+    )
+    with pytest.raises(SystemExit):
+        ClangTidyChecker(config).check()
