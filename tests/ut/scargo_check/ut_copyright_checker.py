5a315e0ec01e89092da56e5f1a681347ee2f4346
Filip Kulic <fku@spyro-soft.com>
2023-12-15 16:54:12 +0100

    Adjust unit tests

--- tests/ut/scargo_check/ut_copyright_checker.py
+++ tests/ut/scargo_check/ut_copyright_checker.py
@@ -1,11 +1,12 @@
+from pathlib import Path
 from typing import Any, List
-from unittest.mock import MagicMock, call
+from unittest.mock import MagicMock
 
 import pytest
 
 from scargo.commands.check import CopyrightChecker
 from scargo.config import Config
-from tests.ut.utils import get_log_data
+from tests.ut.utils import get_log_data, get_test_project_config
 
 FILE_CONTENTS_WITHOUT_COPYRIGHT = [
     "int main(void);",
@@ -19,82 +20,77 @@ FILE_CONTENTS_WITH_COPYRIGHT = [
 ]
 
 FILE_CONTENTS_WITH_INCORRECT_COPYRIGHT = [
-    "// copyright",
+    "// wrong_copyright",
     "int main(void);",
 ]
 
-MISSING_COPYRIGHT_WARNING = ("WARNING", "Missing copyright line in foo/bar.hpp.")
-
 
 @pytest.mark.parametrize(
-    "mock_file_contents",
+    "test_on_tempfile",
     [FILE_CONTENTS_WITH_COPYRIGHT],
     indirect=True,
 )
 def test_check_copyright_pass(
-    mock_file_contents: MagicMock,
     caplog: pytest.LogCaptureFixture,
-    config: Config,
-    mock_find_files: MagicMock,
+    test_on_tempfile: Path,
 ) -> None:
-    result = CopyrightChecker(config).check()
+    test_config = get_test_project_config()
+    result = CopyrightChecker(test_config).check()
 
     assert result == 0
     assert all(level != "WARNING" for level, msg in get_log_data(caplog.records))
 
 
 @pytest.mark.parametrize(
-    "mock_file_contents",
+    "test_on_tempfile",
     [FILE_CONTENTS_WITHOUT_COPYRIGHT, FILE_CONTENTS_WITH_INCORRECT_COPYRIGHT],
     indirect=True,
 )
 def test_check_copyright_fail(
-    mock_file_contents: MagicMock,
+    test_on_tempfile: MagicMock,
     caplog: pytest.LogCaptureFixture,
-    config: Config,
-    mock_find_files: MagicMock,
 ) -> None:
-    result = CopyrightChecker(config).check()
+    test_config = get_test_project_config()
+    result = CopyrightChecker(test_config).check()
 
     assert result == 1
     log_data = get_log_data(caplog.records)
-    assert MISSING_COPYRIGHT_WARNING in log_data
+    assert ("WARNING", f"Missing copyright line in {test_on_tempfile}.") in log_data
 
 
 @pytest.mark.parametrize(
-    "mock_file_contents,expected",
+    "test_on_tempfile,expected",
     [
         (
             FILE_CONTENTS_WITHOUT_COPYRIGHT,
             [
-                call("//\n"),
-                call("// Copyright\n"),
-                call("//\n"),
-                call("\n"),
-                call("int main(void);"),
+                "//\n",
+                "// Copyright\n",
+                "//\n",
+                "\n",
+                "int main(void);",
             ],
         ),
         (
             FILE_CONTENTS_WITH_INCORRECT_COPYRIGHT,
             [
-                call("//\n"),
-                call("// Copyright\n"),
-                call("//\n"),
-                call("\n"),
-                call("// copyright\nint main(void);"),
+                "//\n",
+                "// Copyright\n",
+                "//\n",
+                "\n",
+                "// copyright\nint main(void);",
             ],
         ),
     ],
-    indirect=["mock_file_contents"],
+    indirect=["test_on_tempfile"],
 )
 def test_check_copyright_fix(
-    mock_file_contents: MagicMock,
+    test_on_tempfile: Path,
     expected: List[Any],
-    config: Config,
-    mock_find_files: MagicMock,
 ) -> None:
-    CopyrightChecker(config, fix_errors=True).check()
-    assert mock_file_contents().write.mock_calls == expected
+    test_config = get_test_project_config()
+    CopyrightChecker(test_config, fix_errors=True).check()
+    test_on_tempfile.read_text().splitlines() == expected
 
 
 def test_check_copyright_no_description(
