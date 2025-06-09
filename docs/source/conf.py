46d4bdc5a3d447766a469f2d666d144c356eee2a
Karol Adrianek <kln@spyro-soft.com>
2025-06-09 16:58:58 +0200

    Documentation - UML and miscellaneous fixes (#439)
    * docs/scargo-monitor: fixed warning for too short underline
    
    * docs/conf: ignored all warnings from modules
    
    * docs/scargo_common_seq: bug fix for syntax for participant 'storage'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,9 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns: List[str] = []
+exclude_patterns: List[str] = [
+	r"modules/*.rst" # ignore any warning from modules
+]
 
 # -- Options for HTML output -------------------------------------------------
 
